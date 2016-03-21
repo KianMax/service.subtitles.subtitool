0da6e19a3e4fb05b3e64302a8ad6bd078c2f77e0
KianMax <KianMaxx@gmail.com>
2016-03-21 13:00:51 +0430

    [service.subtitles.subtitool] 1.1.1
    - Small bugfixes
    - Some improvements in the search functionality when you have selected
    multiple languages

--- resources/lib/SubTiTool.py
+++ resources/lib/SubTiTool.py
@@ -12,33 +12,31 @@ class SubTiToolHelper:
         self.md5hash = md5hash
 
     def search(self, item, t, langs):
-        for language in item["3let_language"]:
-            language = "pl" if language == "pol" else language
 
-            fulllang = xbmc.convertLanguage(language, xbmc.ENGLISH_NAME)
-            if fulllang == "Persian": fulllang = "Farsi/Persian"
+        fulllang = xbmc.convertLanguage(item['preferredlanguage'], xbmc.ENGLISH_NAME)
+        if fulllang == "Persian": fulllang = "Farsi/Persian"
+        #xbmc.executebuiltin("Notification(Title," + fulllang + ")")
+        url = "http://www.subtitool.com/api/?query=" + self.filename + "&Lang=" + langs
+        subs = urllib.urlopen(url).read()
+        DOMTree = minidom.parseString(subs)
+        if DOMTree.getElementsByTagName('Subtitle').length == 0:
+           try:
+            url = "http://www.subtitool.com/api/?query=" + self.filename + "&Lang=" + langs + "&OR=1"
+            subs = urllib.urlopen(url).read()
+            DOMTree = minidom.parseString(subs)
+           except Exception, e:
+                log("Subtitool","Not Found OR")
 
+           try:
             url = "http://www.subtitool.com/api/?query=" + self.filename + "&Lang=" + langs
             subs = urllib.urlopen(url).read()
             DOMTree = minidom.parseString(subs)
-            if DOMTree.getElementsByTagName('Subtitle').length == 0:
-                try:
-                    url = "http://www.subtitool.com/api/?query=" + self.filename + "&Lang=" + fulllang + "&OR=1"
-                    subs = urllib.urlopen(url).read()
-                    DOMTree = minidom.parseString(subs)
-                except Exception, e:
-                    log("Subtitool","Not Found OR")
-
-                try:
-                    url = "http://www.subtitool.com/api/?query=" + self.filename + "&Lang=" + fulllang
-                    subs = urllib.urlopen(url).read()
-                    DOMTree = minidom.parseString(subs)
-                except Exception, e:
-                    log("Subtitool","Not Found")
+           except Exception, e:
+                log("Subtitool","Not Found")
 
         return DOMTree
 
-    def download(self, dllink, language="PL"):
+    def download(self, dllink, language="EN"):
 
         try:
             response = urllib.urlopen(dllink)
@@ -48,7 +46,9 @@ class SubTiToolHelper:
 
         try:
             srtdata = response.read()
-            open(self.filename, "w").write(srtdata)
+            with open(self.filename, "w") as file:
+             file.write(srtdata)
+
             return self.filename
 
         except Exception, e:
